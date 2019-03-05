0b764ba13b92d6f6be622d9eeed8121475f68a53
Krzysztof Agieńczuk <225970@student.pwr.edu.pl>
2019-03-05 11:17:41 +0100

    Stream Programming Lab1 change return type

--- Semester 1/Stream Programming/Laboratories/Laboratory 1/src/main/scala/GreatestDivisor.scala
+++ Semester 1/Stream Programming/Laboratories/Laboratory 1/src/main/scala/GreatestDivisor.scala
@@ -4,7 +4,7 @@ import scala.util.control.Breaks
 object GreatestDivisor {
   def main(args: Array[String]){
    try{
-     args.foreach(arg => secondGreatestDivisorSlightlyFaster(arg.toInt))
+     args.foreach(arg => println(secondGreatestDivisorSlightlyFaster(arg.toInt)))
    }catch{
      case exception: NumberFormatException =>
        println("Conversion not possible")
@@ -12,15 +12,15 @@ object GreatestDivisor {
   }
 
   // greatest divisor of a number is the number itself
-  def getDivisor(x: Int): Unit = println(x)
+  def getDivisor(x: Int): Int = x
 
-  def secondGreatestDivisor(x: Int): Unit = {
+  def secondGreatestDivisor(x: Int): Int = {
     var div =0
     for(i <- 1 until x) if (x % i == 0) div = i
-    println(div)
+    div
   }
 
-  def secondGreatestDivisorSlightlyFaster(x: Int): Unit = {
+  def secondGreatestDivisorSlightlyFaster(x: Int): Int = {
     val loop = new Breaks
     var div=1
     loop.breakable {
@@ -31,7 +31,7 @@ object GreatestDivisor {
         }
       }
     }
-    if(div==1) println(1)
-    else println(div)
+    if(div==1) 1
+    else div
   }
 }
