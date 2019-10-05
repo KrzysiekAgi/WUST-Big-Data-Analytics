50f7d0e3b41a17abfa8a70b217446cd2307bd393
Krzysztof Agieńczuk <225970@student.pwr.edu.pl>
2019-10-05 11:31:30 +0200

    Big Data Algorithms Lab 1 List

---
+++ Semester 2/Big Data Algorithms/Lab 1/src/main/scala/WordCount.scala
@@ -0,0 +1,34 @@
+import org.apache.spark.{SparkConf, SparkContext}
+
+object WordCount {
+  def main(args: Array[String]): Unit = {
+
+    val aFew = 3
+
+    val conf: SparkConf = new SparkConf()
+      .setMaster("local[*]")
+      .setAppName("WordCount")
+    val sc: SparkContext = new SparkContext(conf)
+
+    val textFile = sc.textFile("Bible.txt");
+    val stopWordsRaw = sc.textFile("stop.txt")
+    val stopWords = stopWordsRaw.collect.toSet
+    val counts = textFile
+      .flatMap(line => line.split(" "))
+      .map(word => word.toLowerCase)
+      .map(word => word.replaceAll(",|'|\\.|:|;", ""))
+      .filter(word => !stopWords.contains(word) && !word.isEmpty)
+      .map(word => (word, 1))
+      .reduceByKey(_ + _)
+      .map(item => item.swap)
+      .sortByKey(ascending = false, 1)
+
+    //Task said - remove a few dozen of words...
+    val numbers = counts.take(aFew * 12)
+    numbers.foreach(pair => println(pair))
+    val countsRemoved = counts
+      .filter(word => !numbers.contains(word))
+      .filter(word => word._1 != 1)
+    countsRemoved.saveAsTextFile("counts")
+  }
+}
